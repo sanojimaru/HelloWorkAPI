aeaeff3e6e843d5a42a2ae6f219fc9e325e88bd5
Hirohide Sano @sanojimaru <sanojimaru@gmail.com>
2012-10-10 14:32:18 +0900

    fixed ssl connection error.

--- app/batch/hellowork.rb
+++ app/batch/hellowork.rb
@@ -1,3 +1,7 @@
+require 'openssl'
+I_KNOW_THAT_OPENSSL_VERIFY_PEER_EQUALS_VERIFY_NONE_IS_WRONG = nil
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
 require 'hellowork/core'
 
 module Hellowork; end
