24c5573567b9ed82ccc9a383050b6d6c1a35f9a4
Hirohide Sano @sanojimaru <sanojimaru@gmail.com>
2012-12-02 01:51:39 +0900

    fixed prefs bug.

--- app/controllers/prefs_controller.rb
+++ app/controllers/prefs_controller.rb
@@ -1,6 +1,6 @@
 class PrefsController < ApplicationController
   def show
-    @pref = Pref.find_by_name_alnum(params[:pref])
+    @pref = Pref.find_by_name_alnum(params[:pref].classify)
     raise NotFoundException unless @pref
 
     @jobs = Job.where(pref: @pref.name).page params[:page]
