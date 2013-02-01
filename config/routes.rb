9f2c42c7812672efc14618535be66c021a725f65
Hirohide Sano @sanojimaru <sanojimaru@gmail.com>
2013-02-01 16:59:37 +0900

    fixed admin routing bugs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 HelloWorkAPI::Application.routes.draw do
   root to: 'base#index'
+  devise_for :admins
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
   resources :jobs, only: [:show]
   scope ':pref' do
     get '/' => 'prefs#show', as: :pref
   end
-
-  devise_for :admins
-  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 end
