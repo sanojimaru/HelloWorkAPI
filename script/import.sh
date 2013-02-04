3dba890cf1e62a02628d4eaad596a32fe0e1f178
Hirohide Sano @sanojimaru <sanojimaru@gmail.com>
2013-02-04 17:36:06 +0900

    add ruby PATH to import script.

--- script/import.sh
+++ script/import.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-export RAILS_ENV=production && bundle exec rails r "Hellowork::Core.new.run" >> log/import.log 2>&1
+export PATH=/opt/rubies/1.9.3-p194/bin:$PATH
+export RAILS_ENV=production
+
+bundle exec rails r "Hellowork::Core.new.run" >> log/import.log 2>&1
